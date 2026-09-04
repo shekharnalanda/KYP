cfb0270042c5ba8eac29cd0dcd575edec9c0aa6a
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-09-04 17:13:21 +0530

    feat: add KYP assessment foundation

---
+++ config/kyp.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'scoring' => [
+        'exam' => ['raw_max' => 200, 'final_max' => 40],
+        'lab' => ['raw_max' => 200, 'final_max' => 40],
+        'classroom' => ['raw_max' => 100, 'final_max' => 20],
+        'raw_total' => 500,
+        'final_total' => 100,
+    ],
+];

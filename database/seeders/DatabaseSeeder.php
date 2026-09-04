cf35201ef3c196aa491f4917ce4e50a8ed27da35
Sujit SHEKHAR <shekharnalanda@gmail.com>
2026-09-04 17:32:35 +0530

    feat: register assessment seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
-        $this->call(CourseStructureSeeder::class);
+        $this->call([
+            CourseStructureSeeder::class,
+            AssessmentSeeder::class,
+        ]);
     }
 }
